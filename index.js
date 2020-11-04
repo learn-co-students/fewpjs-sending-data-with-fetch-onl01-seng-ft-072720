e53f6a4923ceeeee046473d796bee24180bf6cef
steedn <steedn@outlook.com>
2020-11-03 18:05:03 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(name, email){
+  let names = {name: name, email: email}
+  let obj = {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+      },
+      body: JSON.stringify(names)
+  }
+  return fetch('http://localhost:3000/users', obj)
+  .then(function(response){
+      return response.json()
+  })
+  .then(function(object){
+      document.body.innerHTML = object['id']
+  })
+  .catch(function(error){
+      alert('Bad things happened here...')
+      document.body.innerHTML = error.message
+  })
+}
